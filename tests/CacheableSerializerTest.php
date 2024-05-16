3e2ed037aed70e5cdce74963221098bf709a4445
azandrew-sidoine <azandrewdevelopper@gmail.com>
2024-05-16 17:17:26 +0000

    Fixed test script

--- tests/CacheableSerializerTest.php
+++ tests/CacheableSerializerTest.php
@@ -23,28 +23,17 @@ class CacheableSerializerTest extends TestCase
 {
     public function getPath()
     {
-        return Path(
-            sprintf(
-                '%s%s%s',
-                drewlabs_component_generator_cache_path(),
-                \DIRECTORY_SEPARATOR.'subdir'.\DIRECTORY_SEPARATOR,
-                'dump'
-            )
-        )->canonicalize()->__toString();
+        return realpath(__DIR__ . '/../.cache') . (DIRECTORY_SEPARATOR . 'dump');
     }
 
     public function testDumpMethod()
     {
         $serializer = new Cache($this->getPath());
-        $serializer->dump(new CacheableTables([
-            'tables' => [
-                'auth_users',
-                'auth_user_details',
-                'acl_authorizations',
-            ],
-            'namespace' => 'App',
-            'subNamespace' => 'Auth',
-        ]));
+        $serializer->dump(new CacheableTables(
+            ['auth_users', 'auth_user_details', 'acl_authorizations'],
+            'App',
+            'Auth'
+        ));
         $this->assertTrue(true);
     }
 
